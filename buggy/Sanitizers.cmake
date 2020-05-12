ddf58a7ceb378dee634f79a63e25f2c88389580e
Jude Onyia <judeonyia10@gmail.com>
2020-05-12 00:17:23 -0700

    Rewrote the sections where ENABLE_ASAN and ENABLE_UBSAN are created and given a default value of false

--- buggy/Sanitizers.cmake
+++ buggy/Sanitizers.cmake
@@ -1,12 +1,6 @@
 # The ENABLE_ASAN option sets the Address Sanitizer
-if(NOT DEFINED ENABLE_ASAN)
-	set(ENABLE_ASAN FALSE)
-endif()
-message("ENABLE_ASAN: ${ENABLE_ASAN} ")
-# Error if its not bool
-if(NOT (ENABLE_ASAN MATCHES TRUE OR ENABLE_ASAN MATCHES FALSE))
-	message(FATAL_ERROR "ENABLE_ASAN must be Boolean(TRUE/FALSE)")
-endif()	
+option(ENABLE_ASAN "Enable Asan" false)
+
 if(ENABLE_ASAN)
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
 	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address")
@@ -14,14 +8,8 @@ if(ENABLE_ASAN)
 endif()
 
 # The ENABLE_UBSAN option sets the Undefined Behaviour Sanitizer
-if(NOT DEFINED ENABLE_UBSAN)
-	set(ENABLE_UBSAN FALSE)
-endif()
-message("ENABLE_UBSAN: ${ENABLE_UBSAN} ")
-# Error if its not bool
-if(NOT (ENABLE_UBSAN MATCHES TRUE OR ENABLE_UBSAN MATCHES FALSE))
-	message(FATAL_ERROR "ENABLE_UBSAN must be Boolean(TRUE/FALSE)")
-endif()
+option(ENABLE_UBSAN "Enable UBsan" false)
+
 if(ENABLE_UBSAN)
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
 	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=undefined")
